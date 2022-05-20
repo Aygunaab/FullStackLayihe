3e07c5be98646758c18760958bb00154bc22fe09
Aygunaab <aygunaab@code.edu.az>
2022-05-20 12:26:10 -0400

    front final

--- HandMadeFront/assets/js/main.js
+++ HandMadeFront/assets/js/main.js
@@ -4,8 +4,8 @@ $('.hero-slider').slick({
     autoplay: true,
     autoplaySpeed: 5000,
     arrows:true,
-    prevArrow:(".prev"),
-    nextArrow:(".next"),
+    prevArrow:'<button type="button" class="slick-prev"><i class="pe-7s-angle-left-circle"></i></button>',
+    nextArrow:'<button type="button" class="slick-next"><i class="pe-7s-angle-right-circle"></i></button>',
     fade:true,
     cssEase: 'linear',
     waitForAnimate:true,                  
@@ -17,8 +17,8 @@ $('.hero-slider').slick({
     slidesToShow: 6,
     slidesToScroll: 6, 
     arrows:true,
-    prevArrow:(".prev"),
-    nextArrow:(".next"),
+    prevArrow:'<button type="button" class="slick-prev"><i class="pe-7s-angle-left-circle"></i></button>',
+    nextArrow:'<button type="button" class="slick-next"><i class="pe-7s-angle-right-circle"></i></button>',
     responsive: [
       {
         breakpoint: 1024,
@@ -58,6 +58,9 @@ $('.hero-slider').slick({
     infinite: true,
     slidesToShow: 4,
     slidesToScroll: 4, 
+    arrows:true,
+    prevArrow:'<button type="button" class="slick-prev"><i class="pe-7s-angle-left-circle"></i></button>',
+    nextArrow:'<button type="button" class="slick-next"><i class="pe-7s-angle-right-circle"></i></button>',
     
     responsive: [
       {
@@ -206,5 +209,137 @@ $('.filter-button-group').on( 'click', 'li', function() {
   $grid.isotope({ filter: filterValue });
 });
 
+//product details slider
+const imgs = document.querySelectorAll('.img-select a');
+const imgBtns = [...imgs];
+let imgId = 1;
 
+imgBtns.forEach((imgItem) => {
+    imgItem.addEventListener('click', (event) => {
+        event.preventDefault();
+        imgId = imgItem.dataset.id;
+        slideImage();
+    });
+});
+
+function slideImage(){
+    const displayWidth = document.querySelector('.img-shopcase img:first-child').clientWidth;
+
+    document.querySelector('.img-shopcase').style.transform = `translateX(${- (imgId - 1) * displayWidth}px)`;
+}
+
+//product details tabs
+$( function() {
+  $( "#product-details-tabs" ).tabs({
+    collapsible: true
+  });
+} );
+
+
+
+  //Cart Plus Minus Button
+    var CartPlusMinus = $(".cart-plus-minus");
+    CartPlusMinus.prepend('<div class="minus plmin">-</div>');
+    CartPlusMinus.append('<div class="plus plmin">+</div>');
+    $(".plmin").on("click", function () {
+        var $button = $(this);
+        var oldValue = $button.parent().find("input").val();
+        if ($button.text() === "+") {
+            var newVal = parseFloat(oldValue) + 1;
+        } else {
+            // Don't allow decrementing below zero
+            if (oldValue > 1) {
+                var newVal = parseFloat(oldValue) - 1;
+            } else {
+                newVal = 1;
+            }
+        }
+        $button.parent().find("input").val(newVal);
+    });
+
+    //Related Products slide
+
+    $('.relatedProducts').slick({
+      infinite: true,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 3000,
+      arrows:true,
+      prevArrow:(".prev"),
+      nextArrow:(".next"),   
+      responsive: [
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,    
+          }
+        },
+        {
+          breakpoint: 900,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          }
+        }
+      
+      ]
+    });
+    //fag accordion
+    $( function() {
+      $( "#fag-accordion" ).accordion({
+        collapsible: true
+      });
+      
+    } );
+
+    // login tab
+    $( function() {
+      $( "#login-reg" ).tabs({
+        collapsible: true
+      });
+    } );
+
+
+    //checkbox toogle 
+    function Cheked() {
+      var checkBox = document.getElementById("CheckInput");
+      
+      var account = document.getElementById("chec-box");
+    
+      // If the  is checked, display the output text
+      if (checkBox.checked == true){
+        account.style.display = "block";
+      } else {
+        account.style.display = "none";
+      }
+    }
+    
+
+    function ChekedAdress() {
+      var checkBox = document.getElementById("CheckAuthorAdress");
+      
+      var account = document.getElementById("different-adress");
+    
+      // If the  is checked, display the output text
+      if (checkBox.checked == true){
+        account.style.display = "block";
+      } else {
+        account.style.display = "none";
+      }
+    }
+    //order accordion
+    $( function() {
+      $( "#order" ).accordion({
+        collapsible: true
+      });
+    } );
 
